9a9e26bb680abab5c7fd8f127877a13cca32fd98
Никитос <nikitos@MacBook-Air-Tana.local>
2025-09-13 23:36:05 +0300

    Add another hash_func

--- book_module/books_map.py
+++ book_module/books_map.py
@@ -7,10 +7,18 @@ class BooksMap(HashMap):
         super().__init__(size)
 
     #Хэш-функция
+    '''Хэш-функция для реального isbn
     def hash_func(self, isbn):
         clean_isbn = "".join(c for c in isbn if c.isdigit() or c == "-")
         selected = list(map(int, clean_isbn.split("-")))
         hash_value = (selected[1] << 8) + selected[2] * 31 + selected[3] % 17 + selected[4]
+        return hash_value % self.size'''
+    
+    '''Хэш-функция для любой строки, в качестве isbn'''
+    def hash_func(self, isbn):
+        hash_value = 0
+        for char in isbn:
+            hash_value = (hash_value * 31 + ord(char))
         return hash_value % self.size
 
     #Добавление книги
