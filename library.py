e855cdd4fe2dd4f544dec9b953af86ea1cd2b699
Никитос <nikitos@MacBook-Air-Tana.local>
2025-09-13 22:38:00 +0300

    Make imports

--- library.py
+++ library.py
@@ -1,6 +1,11 @@
+from book_module.book import Book
 from book_module.books_map import BooksMap
-from user_module.users_map import UsersMap
 from borrowing_record import BorrowingRecord
+from user_module.user_types.faculty import Faculty
+from user_module.user_types.guest import Guest
+from user_module.user_types.student import Student
+from user_module.users_map import UsersMap
+
 
 
 class Library:
@@ -9,5 +14,3 @@ class Library:
         self.users = UsersMap()
         self.borrowing_history = []
         self.genres = set()
-    
-        
