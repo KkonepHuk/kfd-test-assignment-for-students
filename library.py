f7faa6533ff417c3ad600a0f09613c2176dd37b8
Никитос <nikitos@MacBook-Air-Tana.local>
2025-09-13 22:38:41 +0300

    Add borrow_book func

--- library.py
+++ library.py
@@ -14,3 +14,23 @@ class Library:
         self.users = UsersMap()
         self.borrowing_history = []
         self.genres = set()
+
+    def borrow_book(self, user_id, isbn):
+        user = self.users.get(user_id)
+        book = self.books.get(isbn)
+
+
+        #Проверяем возможность взять книгу
+        if user == -1 or book == -1: #Существуют ли книга и пользователь
+            return False
+        if not book.is_available(): #Доступна ли книга
+            return False
+        if not user.can_borrow(): #Может ли пользователь взять книгу
+            return False
+        
+
+        book.set_available(False)
+        user.get_borrowed_books().append(isbn)
+        self.borrowing_history.append(BorrowingRecord(user, book))
+
+        return True
