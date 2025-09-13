7dd04d23b061801a720cf41652d40ff643854c97
Никитос <nikitos@MacBook-Air-Tana.local>
2025-09-13 19:45:01 +0300

    HashMap auto-expansion

--- hashmap/hash_map.py
+++ hashmap/hash_map.py
@@ -16,3 +16,21 @@ class HashMap:
     #Вычисление коэффициента заполнения таблицы
     def occupancy_rate(self):
         return self.occupancy / self.size if self.size > 0 else 0
+    
+    @staticmethod
+    def expansion(add_func):
+        def wrapper(self, *args):
+            if self.occupancy_rate() >= 2 / 3:
+                new_size = self.size + int(self.size * 2 / 3)
+                new_library_system = HashMap(new_size)
+                for sll in self.table:
+                    while sll and sll.head:
+                        node = sll.remove_from_start()
+                        item = node.item
+                        new_library_system.add(item)
+                self.size = new_library_system.size
+                self.table = new_library_system.table
+                self.occupancy = new_library_system.occupancy
+
+            return add_func(self, *args)
+        return wrapper
