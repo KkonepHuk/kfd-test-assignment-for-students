eeba8b7b4139c77287999293c46ab1f51e6b7648
Никитос <nikitos@MacBook-Air-Tana.local>
2025-09-13 19:59:32 +0300

    Add adding func to BooksMap

--- books_map.py
+++ books_map.py
@@ -1,4 +1,5 @@
 from hashmap.hash_map import HashMap
+from hashmap.singly_linked_list import SinglyLinkedList
 
 
 class BooksMap(HashMap):
@@ -11,3 +12,15 @@ class BooksMap(HashMap):
         selected = list(map(int, clean_isbn.split("-")))
         hash_value = (selected[1] << 8) + selected[2] * 31 + selected[3] % 17 + selected[4]
         return hash_value % self.size
+
+    #Добавление книги
+    @HashMap.expansion
+    def add(self, book):
+        ind = self.hash_func(book.isbn)
+        if self.table[ind] == None:
+            sll = SinglyLinkedList()
+            sll.add_to_start(book)
+            self.table[ind] = sll
+        else:
+            self.table[ind].add_to_start(book)
+        self.incr_occupation()
