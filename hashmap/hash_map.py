8bdc34062afd11cc30ce2df5482b494053dff2db
Никитос <nikitos@MacBook-Air-Tana.local>
2025-09-13 19:43:36 +0300

    HashMap occupancy system

--- hashmap/hash_map.py
+++ hashmap/hash_map.py
@@ -4,3 +4,15 @@ class HashMap:
         self.size = size
         self.table = [None] * size
         self.occupancy = 0
+
+    #Увеличение на 1 счётчика заполненных ячеек Хэш-Таблицы
+    def incr_occupation(self):
+        self.occupancy += 1
+    
+    #Уменьшение на 1 счётчика заполненных ячеек Хэш-Таблицы
+    def decr_occupation(self):
+        self.occupancy -= 1
+    
+    #Вычисление коэффициента заполнения таблицы
+    def occupancy_rate(self):
+        return self.occupancy / self.size if self.size > 0 else 0
