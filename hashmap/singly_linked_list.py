8a045319077f3abcef892fd9c193141693358a7f
Никитос <nikitos@MacBook-Air-Tana.local>
2025-09-13 19:34:05 +0300

    SinglyLinkedList init

--- hashmap/singly_linked_list.py
+++ hashmap/singly_linked_list.py
@@ -3,3 +3,19 @@ class Node:
         self.key = key
         self.item = item
         self.next = None
+
+class SinglyLinkedList:
+
+    #Инициализация односвязного списка
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    #Строковый формат
+    def __str__(self):
+        current = self.head
+        s = ''
+        while current:
+            s += f'{str(current.item)}->'
+            current = current.next
+        return s[:-2]
